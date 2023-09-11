b7f538b4b5a93994879e14cdca3849382c997f1c
Adam <cmlee3211@gmail.com>
2023-09-10 17:51:57 -0700

    Save in json

--- generate.py
+++ generate.py
@@ -17,40 +17,40 @@ def main():
     
     clean_input_name = input_filename.split(".")[0]
     
-    with open(f'output/{clean_input_name}.csv', 'w', newline='') as csvfile:
-        columns = ['api_name', 'query', 'model_answer']
-        writer = csv.DictWriter(csvfile, fieldnames=columns)
-        writer.writeheader()
+    output_data = []
         
-        print(f"Length of data: {len(data)}")
+    print(f"Length of data: {len(data)}")
+    
+    for index, example in enumerate(tqdm(data)):
+        print(f"Example {index+1}/{len(data)}")
+        example_str = json.dumps(example)
         
-        for index, example in enumerate(tqdm(data)):
-            print(f"Example {index}/{len(data)}")
-            example_str = json.dumps(example)
-            
-            domain = example['domain']
-            framework = example['framework']
-            functionality = example['functionality']
-            name = example['api_name']
-            call = example['api_call']
-            arguments = example['api_arguments']
-            metadata = json.dumps(example['meta_data'])
-            
-            prompt = template.template.format(example, example_str)
-            
-            response = OpenAI_API.chatgpt(prompt)
-            print('\t', response)
-            
-            user_query = response.split("\n")[0].split(": ")[1].strip()
-            model_answer = response.split("\n")[1].split(": ")[1].strip()
-            
-            sample_dict = {
-                'api_name': name,
-                'query': user_query,
-                'model_answer': model_answer
-            }
-            
-            writer.writerow(sample_dict)
+        domain = example['domain']
+        framework = example['framework']
+        functionality = example['functionality']
+        name = example['api_name']
+        call = example['api_call']
+        arguments = example['api_arguments']
+        metadata = json.dumps(example['meta_data'])
+        
+        prompt = template.template.format(example, example_str)
+        
+        response = OpenAI_API.chatgpt(prompt)
+        print(response)
+        
+        user_query = response.split("\n")[0].split(": ")[1].strip()
+        model_answer = response.split("\n")[1].split(": ")[1].strip()
+        
+        sample_dict = {
+            'api_name': name,
+            'query': user_query,
+            'model_answer': model_answer
+        }
+        
+        output_data.append(sample_dict)
+    
+    with open(f'output/{clean_input_name}.json', 'w') as jsonfile:
+        json.dump(output_data, jsonfile, indent=4)
 
 
 if __name__ == "__main__":
