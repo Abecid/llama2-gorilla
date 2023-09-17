41cda24963c2a837c2d5bd69ab41581b9a8b5c98
Adam <cmlee3211@gmail.com>
2023-09-17 02:50:28 -0700

    Add try else continue in case of error

--- generate.py
+++ generate.py
@@ -30,44 +30,58 @@ def generate_queries_for_file(data, clean_input_name):
         if index == MAX_PER_FILE:
             break
         print(f"Example {index+1}/{len(data)}")
-        example_str = json.dumps(example)
         
-        domain = example['domain']
-        framework = example['framework']
-        functionality = example['functionality']
-        name = example['api_name']
-        call = example['api_call']
-        arguments = example['api_arguments']
-        # metadata = json.dumps(example['meta_data'])
-        
-        prompt = template.template.format(example, example_str)
-        if MULTI_OUTPUT > 1:
-            prompt = template.multi_output_template.format(example, example_str, MULTI_OUTPUT)
-        
-        response = OpenAI_API.chatgpt(prompt)
-        print(response)
-        
-        if MULTI_OUTPUT > 1:
-            # Find the start positions of each "User query<n>"
-            positions = [match.start() for match in re.finditer(r'User query\d+', response)]
-            # Extract substrings based on these start positions
-            responses = [response[positions[i]:positions[i+1]] if i < len(positions) - 1 else response[positions[i]:] for i in range(len(positions))]
-            # responses = response.split("\n\n")
+        try:
+            example_str = json.dumps(example)
             
-            for i in range(len(responses)):
-                response = responses[i]
-                user_query = response.split("\n")[0].split(": ")[1].strip()
-                model_answer = response.split("\n")[1].split(": ")
-                if len(model_answer) > 2: model_answer = model_answer[1].strip()
-                else: model_answer = ""
-                if len(response.split("\n")) > 2:
-                    model_answer2 = "\n".join(response.split("\n")[2:])
-                    model_answer = f"{model_answer}\n{model_answer2}".strip()
-                    # if len(model_answer) == 0:
-                    #     model_answer = model_answer2
-                    # else:
-                    #     model_answer = model_answer + "\n" + model_answer2
+            domain = example['domain']
+            framework = example['framework']
+            functionality = example['functionality']
+            name = example['api_name']
+            call = example['api_call']
+            arguments = example['api_arguments']
+            # metadata = json.dumps(example['meta_data'])
+            
+            prompt = template.template.format(example, example_str)
+            if MULTI_OUTPUT > 1:
+                prompt = template.multi_output_template.format(example, example_str, MULTI_OUTPUT)
+            
+            response = OpenAI_API.chatgpt(prompt)
+            print(response)
+            
+            if MULTI_OUTPUT > 1:
+                # Find the start positions of each "User query<n>"
+                positions = [match.start() for match in re.finditer(r'User query\d+', response)]
+                # Extract substrings based on these start positions
+                responses = [response[positions[i]:positions[i+1]] if i < len(positions) - 1 else response[positions[i]:] for i in range(len(positions))]
+                # responses = response.split("\n\n")
+                
+                for i in range(len(responses)):
+                    response = responses[i]
+                    user_query = response.split("\n")[0].split(": ")[1].strip()
+                    model_answer = response.split("\n")[1].split(": ")
+                    if len(model_answer) > 1: model_answer = model_answer[1].strip()
+                    else: model_answer = ""
+                    if len(response.split("\n")) > 2:
+                        model_answer2 = "\n".join(response.split("\n")[2:])
+                        model_answer = f"{model_answer}\n{model_answer2}".strip()
+                        # if len(model_answer) == 0:
+                        #     model_answer = model_answer2
+                        # else:
+                        #     model_answer = model_answer + "\n" + model_answer2
+                        
                     
+                    sample_dict = {
+                        # 'api_name': name,
+                        'query': user_query,
+                        'model_answer': model_answer,
+                        'original': example
+                    }
+                    
+                    output_data.append(sample_dict)
+            else:
+                user_query = response.split("\n")[0].split(": ")[1].strip()
+                model_answer = response.split("\n")[1].split(": ")[1].strip()
                 
                 sample_dict = {
                     # 'api_name': name,
@@ -77,18 +91,9 @@ def generate_queries_for_file(data, clean_input_name):
                 }
                 
                 output_data.append(sample_dict)
-        else:
-            user_query = response.split("\n")[0].split(": ")[1].strip()
-            model_answer = response.split("\n")[1].split(": ")[1].strip()
-            
-            sample_dict = {
-                # 'api_name': name,
-                'query': user_query,
-                'model_answer': model_answer,
-                'original': example
-            }
-            
-            output_data.append(sample_dict)
+        except Exception as e:
+            print(e)
+            continue
     now = datetime.now()
     # Convert to string format
     date_string = now.strftime('%m_%d')
